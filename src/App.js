667a32e85848b10715f313cb9a41b6c208ecccb4
aswarth <m.aswarthreddy@gmail.com>
2023-03-08 12:21:13 +0530

    rewrite everything

--- src/App.js
+++ src/App.js
@@ -1,83 +1,185 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
 
 
-class node{
-  constructor(data){
-    this.data = data
-    this.next = null
-    this.prev = null
+class node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+    this.prev = null;
+    this.addr = Math.floor(1000 + Math.random() * 9000);
   }
 }
 
-function App() {
-  var bo = new node(5)
-  bo.next = new node(9)
-
-  const [root, setRoot] = React.useState([bo])
-  const dataRef = React.useRef()
-  console.log(root)
-  var value = 4
-
-
-
-  function addNode({root}){
-    const newRoot = root
-    var [temp] = newRoot
-    while(temp.next){
-      temp=temp.next
-    }
-    console.log(dataRef.current.value)
-    temp.next = new node(dataRef.current.value)
-    setRoot([...newRoot])
-    console.log(newRoot)
-  }
-
+function Node({ value, addr, color }) {
   return (
     <>
-      <div className="container">
-        <List root={root}/>
+      <div className={`node ${color}`}>
+        <div className="data">{value}</div>
+        <div className="addr">{addr}</div>
       </div>
-      <input className="dataInput" type="number" defaultValue="0" ref={dataRef} />
-      <div className="add" role="button" onClick={() => addNode({root})}>Add</div>
     </>
   );
 }
 
-function Node({data, arrow}){
-  console.log(data)
-  return(
-    <div>
-      {data}
-      {arrow?" ->":""}
-    </div>
-  )
+function Arrow() {
+  return <div className="arrow"><FontAwesomeIcon icon={solid('arrow-right-long')} size={"2x"} /></div>;
 }
 
+function App() {
+  var bo = new node(5);
+  bo.next = new node(9);
 
-const List = ({ root }) => {
-  
-  function getNodes({root}){
-    var nodes = [];
-    var [temp] = root
-    while(temp.next){
-      nodes.push(<Node data={temp.data} arrow={true}/>)
-      temp = temp.next
+  const [linkedList, setLinkedList] = useState([]);
+  const [nodeValue, setNodeValue] = useState();
+  const [start, setStart] = useState();
+  const [inputData, setInputData] = useState([1, 2]);
+  const [pointers, setPointers] = useState({});
+  const dataRef = useRef();
+
+  useEffect(() => {
+    handleData();
+  }, []);
+
+  useEffect(() => {
+    setLinkedList([]);
+    parseLL(start);
+  }, [start]);
+
+  useEffect(() => {
+    setLinkedList([...linkedList, nodeValue]);
+  }, [nodeValue]);
+
+  useEffect(() => {
+    setLinkedList([]);
+    parseLL2(start);
+  }, [pointers]);
+
+  function handleData() {
+    if (inputData !== "") {
+      let nodeList = inputData.map((value) => {
+        return new node(value);
+      });
+      for (let start = 0; start < nodeList.length; start++) {
+        if (start !== nodeList.length - 1) {
+          nodeList[start].next = nodeList[start + 1];
+        }
+      }
+      nodeList[nodeList.length - 1].next = null;
+      setStart(nodeList[0]);
+      setInputData("");
     }
-    nodes.push(<Node data={temp.data} arrow={false}/>)
-    console.log(nodes)
-    return(nodes.map((data, i) => {
-      console.log(data)
-      return(
-        <div className="node">{data}</div>
-      )
-    }))
   }
 
-  return(
-      getNodes({root})
-  )
-}
+  async function parseLL(temp) {
+    while (temp != null) {
+      var nodeValue = await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(
+            <>
+              <Node
+                value={temp.data}
+                addr={temp.addr}
+                color={
+                  temp === pointers.current
+                    ? "current"
+                    : temp === pointers.post
+                    ? "post"
+                    : ""
+                }
+              />
+              {temp.next !== null ? <Arrow /> : ""}
+            </>
+          );
+        }, 200);
+      });
+      setNodeValue(nodeValue);
+      temp = temp.next;
+    }
+  }
 
+  function parseLL2(temp) {
+    var result = [];
+    while (temp != null) {
+      var nodeValue = (
+        <>
+          <Node
+            value={temp.data}
+                addr={temp.addr}
+            color={
+              temp === pointers.current
+                ? "current"
+                : temp === pointers.post
+                ? "post"
+                : ""
+            }
+          />
+          {temp.next !== null ? <Arrow /> : ""}
+        </>
+      );
+      result.push(nodeValue);
+      temp = temp.next;
+    }
+    setLinkedList(result);
+  }
+
+  async function addNode() {
+    var current = null;
+    var post = start;
+    setPointers({ current, post });
+    while (post != null) {
+      var pointers = await new Promise((resolve) => {
+        setTimeout(() => {
+          current = post;
+          post = current.next;
+          resolve({ current, post });
+        }, 1000);
+      });
+      setPointers(pointers);
+    }
+    var newNode = new node(dataRef.current.value);
+    current.next = newNode;
+    var nodeValue = (
+      <>
+        <Node
+          value={newNode.data}
+          addr={0}
+          color={
+            newNode === pointers.current
+              ? "current"
+              : newNode === pointers.post
+              ? "post"
+              : ""
+          }
+        />
+        {newNode.next !== null ? <Arrow /> : ""}
+      </>
+    );
+    await new Promise((res) => setTimeout(res, 1000));
+    setNodeValue(nodeValue);
+    setPointers({});
+  }
+
+  return (
+    <>
+      <div className="inputContainer">
+        <input
+          className="dataInput"
+          type="number"
+          defaultValue="0"
+          ref={dataRef}
+        />
+        <div className="add" role="button" onClick={() => addNode()}>
+          Add
+        </div>
+      </div>
+      <div className="linkedList">
+        {linkedList && linkedList.map((node) => node)}
+      </div>
+    </>
+  );
+}
 
 export default App;
